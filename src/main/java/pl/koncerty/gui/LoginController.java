fb2812fe8af344af88c3ce91fea8b9ac53c78916
Akacz123 <100wiktor@gmail.com>
2025-05-12 10:26:15 +0200

    dodalem logowanie uzytkownikow

--- src/main/java/pl/koncerty/gui/LoginController.java
+++ src/main/java/pl/koncerty/gui/LoginController.java
@@ -3,10 +3,14 @@ package pl.koncerty.gui;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
+import org.hibernate.Session;
+import pl.koncerty.HibernateUtil;
+import pl.koncerty.model.Uzytkownik;
+import org.hibernate.query.Query;
 
 public class LoginController {
 
@@ -34,16 +38,40 @@ public class LoginController {
             }
             return;
         }
+        MenuItem statusLabel = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Uzytkownik> query = session.createQuery(
+                    "FROM Uzytkownik WHERE login = :login AND haslo = :haslo", Uzytkownik.class);
+            query.setParameter("login", login);
+            query.setParameter("haslo", haslo);
+            Uzytkownik uzytkownik = query.getSingleResult();
 
+            if (uzytkownik != null) {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/pl/koncerty/gui/uzytkownik_panel.fxml"));
+                Scene scene = new Scene(loader.load());
+                Stage stage = new Stage();
+                stage.setTitle("Panel użytkownika");
+                stage.setScene(scene);
+                stage.show();
+
+                Stage currentStage = (Stage) loginField.getScene().getWindow();
+                currentStage.close();
+            } else {
+                statusLabel.setText("Nieprawidłowy login lub hasło.");
+                statusLabel.setStyle("-fx-text-fill: red;");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            statusLabel.setText("Błąd logowania.");
+        }
         System.out.println("Login lub hasło niepoprawne.");
     }
-
     @FXML
     private void przejdzDoRejestracji() {
         try {
-            javafx.fxml.FXMLLoader loader = new javafx.fxml.FXMLLoader(getClass().getResource("/pl/koncerty/gui/rejestracja.fxml"));
-            javafx.scene.Scene scene = new javafx.scene.Scene(loader.load());
-            javafx.stage.Stage stage = new javafx.stage.Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/pl/koncerty/gui/rejestracja.fxml"));
+            Scene scene = new Scene(loader.load());
+            Stage stage = new Stage();
             stage.setTitle("Rejestracja użytkownika");
             stage.setScene(scene);
             stage.show();
@@ -51,5 +79,5 @@ public class LoginController {
             e.printStackTrace();
         }
     }
-
 }
+
